598ae8cb4b2946f34a22891e79c97b04cedeafcc
yousirong <diziyong1523@gmail.com>
2024-11-05 16:22:10 +0900

    20241105

--- brdit/run_g.sh
+++ brdit/run_g.sh
@@ -5,4 +5,4 @@ params=" \
             --rope-img base512 \
             --rope-real \
             "
-deepspeed hydit/train_deepspeed.py ${params}  "$@"
+deepspeed brdit/train_deepspeed.py ${params}  "$@"
